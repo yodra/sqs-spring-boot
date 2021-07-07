f7c700f6de85955edcccae83463b09501ff463fe
yodralopez <yodralopez@gmail.com>
2021-07-07 12:45:31 +0100

    comment dependencies to work it

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.amazonaws:aws-java-sdk-sqs:1.12.17")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.cloud:spring-cloud-starter-aws:$springCloudVersion")
-    implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:$springCloudVersion")
+//    implementation("com.amazonaws:aws-java-sdk-sqs:1.12.17")
+//    implementation("org.springframework.boot:spring-boot-starter-web")
+//    implementation("org.springframework.cloud:spring-cloud-starter-aws:$springCloudVersion")
+//    implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:$springCloudVersion")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
